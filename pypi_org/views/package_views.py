900a1dfd4000e88555537e109167a44243094946
Will Schlitzer <schlitzer90@gmail.com>
2020-07-20 13:38:17 +0100

    Start over

--- pypi_org/views/package_views.py
+++
@@ -1,9 +0,0 @@
-import flask
-import pypi_org.services.package_service as package_service
-
-blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
-
-@blueprint.route('/project/<package_name>')
-def package_details(package_name: str):
-    #return flask.render_template('packages/details.html', "Package details for {}".format(package_name))
-    return "Package details for {}".format(package_name)

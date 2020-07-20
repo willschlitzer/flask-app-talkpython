936eb023c6ec3301fa4a8f4281eedadbfdb5026e
Will Schlitzer <schlitzer90@gmail.com>
2020-07-20 13:36:47 +0100

    Placeholder pre-starting over

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -4,7 +4,6 @@ import pypi_org.services.package_service as package_service
 blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 @blueprint.route('/project/<package_name>')
-def index(package_name: str):
-    test_packages = package_service.get_latest_packages()
+def package_details(package_name: str):
     #return flask.render_template('packages/details.html', "Package details for {}".format(package_name))
     return "Package details for {}".format(package_name)

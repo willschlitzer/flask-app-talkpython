965c0baf2a1e040fe072cae62f7a9f062e238c01
Will Schlitzer <schlitzer90@gmail.com>
2020-07-22 20:45:32 +0100

    Move page info to home_views.py

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,19 +1,16 @@
 import flask
-import pypi_org.services.package_service as package_service
 
 app = flask.Flask(__name__)
 
+def main():
+    register_blueprints()
+    app.run(debug=True)
 
-@app.route("/")
-def index():
-    test_packages = package_service.get_latest_packages()
-    return flask.render_template("home/index.html", packages=test_packages)
-
-
-@app.route("/about")
-def about():
-    return flask.render_template("home/about.html")
+def register_blueprints():
+    from pypi_org.views import home_views
+    app.register_blueprint(home_views.blueprint)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
+

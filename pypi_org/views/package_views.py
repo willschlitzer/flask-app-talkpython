6a8b17f7c9bb903b88d6ea8b78b01af9418e2269
Will Schlitzer <schlitzer90@gmail.com>
2020-07-23 15:11:28 +0100

    Add popular function

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -7,3 +7,7 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 def package_details(package_name: str):
     #return flask.render_template("home/index.html", packages=test_packages)
     return "Package details for {}".format(package_name)
+
+@blueprint.route("/<int:rank>")
+def popular(rank: int):
+    return "The details for the {}th most popular package.".format(rank)

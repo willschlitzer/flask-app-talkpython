a0168b02a590cb5130c8507de68bcedb1857a81d
Will Schlitzer <schlitzer90@gmail.com>
2020-10-13 13:49:57 +0100

    Deleting files to start project over

--- pypi_org/services/package_service.py
+++
@@ -1,6 +0,0 @@
-def get_latest_packages():
-    return [
-        {'name': 'flask', 'version':'1.2.3'},
-        {'name': 'sqlalchemy', 'version': '2.2.0'},
-        {'name': 'passlib', 'version': '3.0.0'},
-    ]

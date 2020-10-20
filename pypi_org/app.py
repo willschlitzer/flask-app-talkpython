6eeb679b92c268e76fb07b1c7fd4923e04646d4e
Will Schlitzer <schlitzer90@gmail.com>
2020-10-20 18:15:27 +0100

    Move database simulating function to package_service.py

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,22 +1,19 @@
 import flask
 
 app = flask.Flask(__name__)
+import pypi_org.services.package_service as package_service
 
-def get_latest_packages():
-    return [
-        {'name': 'flask', 'version': '1.2.3'},
-        {'name': 'sqlalchemy', 'version': '2.2.0'},
-        {'name': 'passlib', 'version': '3.0.0'},
-    ]
 
 @app.route("/")
 def index():
-    test_packages = get_latest_packages()
-    return flask.render_template('home/index.html', packages=test_packages)
+    test_packages = package_service.get_latest_packages()
+    return flask.render_template("home/index.html", packages=test_packages)
+
 
 @app.route("/about")
 def about():
-    return flask.render_template('home/about.html')
+    return flask.render_template("home/about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)

b02b363c1dd5cef83644cc8e6dd88aa9874b08c9
Will Schlitzer <schlitzer90@gmail.com>
2020-10-23 06:52:33 +0100

    Delete info on Home page and nav bar; per class

--- pypi_org/views/cms_views.py
+++ pypi_org/views/cms_views.py
@@ -6,7 +6,7 @@ blueprint = flask.Blueprint('cms', __name__, template_folder='templates')
 
 @blueprint.route("/<path:full_url>")
 def cms_page(full_url: str):
-    print("Getting CMS page for {}".format(full_url))
+    #print("Getting CMS page for {}".format(full_url))
     page = cms_service.get_page(full_url)
     if not page:
         return flask.abort(404)

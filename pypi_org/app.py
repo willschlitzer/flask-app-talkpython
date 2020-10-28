1cd300a73995e5d9b6327b829385f5f1ef891239
Will Schlitzer <schlitzer90@gmail.com>
2020-10-28 08:28:57 +0000

    Create db_session.py

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,10 +1,22 @@
 import flask
+import os
+import pypi_org.data.db_session as db_session
 
 app = flask.Flask(__name__)
 
 def main():
     register_blueprints()
+    setup_db()
     app.run(debug=True)
+
+def setup_db():
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        "pypi.sqlite")
+
+    db_session.global_init(db_file)
+
 
 def register_blueprints():
     from pypi_org.views import home_views

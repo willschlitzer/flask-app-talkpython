ab98f49ad7db4b459dfdc6ec5a19d002b1b9d21d
Will Schlitzer <schlitzer90@gmail.com>
2020-11-11 06:32:09 +0000

    Add basic_inserts.py

---
+++ pypi_org/bin/basic_inserts.py
@@ -0,0 +1,34 @@
+import os
+import pypi_org
+import pypi_org.data.db_session as db_session
+from pypi_org.data.package import Package
+
+def main():
+    init_db()
+    while True:
+        insert_a_package()
+
+def insert_a_package():
+    p = Package()
+    p.id = input("Package id/name: ").strip().lower()
+    p.summary = input("Package summary: ").strip()
+    p.author = input("Author: ").strip()
+    p.license = input("License: ").strip()
+
+    import sqlalchemy.orm
+    session: sqlalchemy.orm.Session = db_session.factory()
+
+    session.add(p)
+
+
+    session.commit()
+
+def init_db():
+    top_folder = os.path.dirname(__file__)
+    rel_file = os.path.join("..", 'db', 'pypi.sqlite')
+    db_file = os.path.join(top_folder, rel_file)
+    db_session.global_init(db_file)
+
+
+if __name__ == "__main__":
+    main()

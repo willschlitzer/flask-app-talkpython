be459a050a8bce7418971058609589f7324a64ab
Will Schlitzer <schlitzer90@gmail.com>
2020-11-11 11:02:25 +0000

    Create create_session function

--- pypi_org/bin/basic_inserts.py
+++ pypi_org/bin/basic_inserts.py
@@ -32,12 +32,8 @@ def insert_a_package():
     r.size = int(input("Size in bytes: "))
     p.releases.append(r)
 
-    import sqlalchemy.orm
-    session: sqlalchemy.orm.Session = db_session.factory()
-
+    session = db_session.create_session()
     session.add(p)
-
-
     session.commit()
 
 def init_db():
--- pypi_org/data/db_session.py
+++ pypi_org/data/db_session.py
@@ -1,13 +1,14 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
 
 from pypi_org.data.modelbase import SqlAlchemyBase
 
-factory = None
+__factory = None
 
 def global_init(db_file: str):
-    global factory
-    if factory:
+    global __factory
+    if __factory:
         return
 
     if not db_file or not db_file.strip():
@@ -18,9 +19,13 @@ def global_init(db_file: str):
 
     engine = sa.create_engine(conn_str, echo=False)
 
-    factory = orm.sessionmaker(bind=engine)
+    __factory = orm.sessionmaker(bind=engine)
 
     # no inspection
     import pypi_org.data.__all_models
 
     SqlAlchemyBase.metadata.create_all(engine)
+
+def create_session() -> Session:
+    global __factory
+    return __factory()

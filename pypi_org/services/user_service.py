e6b424371dd38e6bc2ea9e0a1435a59868e25b96
Will Schlitzer <schlitzer90@gmail.com>
2020-11-14 11:41:57 +0000

    Create queries for package names

--- pypi_org/services/user_service.py
+++ pypi_org/services/user_service.py
@@ -2,5 +2,6 @@ import pypi_org.data.db_session as db_session
 from pypi_org.data.users import User
 
 def get_user_count() -> int:
+    return 200
     session = db_session.create_session()
     return session.query(User).count()
--- pypi_org/views/home_views.py
+++ pypi_org/views/home_views.py
@@ -8,9 +8,9 @@ blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 
 @blueprint.route("/")
 def index():
-    test_packages = package_service.get_latest_packages()
+    test_packages = package_service.get_latest_releases()
     return flask.render_template("home/index.html",
-                                 packages=test_packages,
+                                 packages=package_service.get_latest_releases(),
                                  package_count=package_service.get_package_count(),
                                  release_count=package_service.get_release_count(),
                                  user_count=user_service.get_user_count())

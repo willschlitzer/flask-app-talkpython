50018da777a8a387275a7e49c54a1fcfb349679e
Will Schlitzer <schlitzer90@gmail.com>
2020-11-14 08:11:32 +0000

    Update for querying database for package info

--- pypi_org/services/package_service.py
+++ pypi_org/services/package_service.py
@@ -1,13 +1,18 @@
+from typing import List
+import sqlalchemy.orm
+
 import pypi_org.data.db_session as db_session
 from pypi_org.data.package import Package
 from pypi_org.data.releases import Release
 
-def get_latest_packages():
-    return [
-        {"name": "flask", "version": "1.2.3"},
-        {"name": "sqlalchemy", "version": "2.2.0"},
-        {"name": "passlib", "version": "3.0.0"},
-    ]
+def get_latest_releases(limit = 10) -> List[Release]:
+    session = db_session.create_session()
+
+    releases = session.query(Release).\
+        order_by(Release.created_date.desc()).\
+        limit(limit).\
+        all()
+    return releases
 
 def get_package_count() -> int:
 
